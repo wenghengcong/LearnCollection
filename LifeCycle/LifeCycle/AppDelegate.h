634578d3c7126cda3957a80ecdaf91fdc04a0e5d
wenghengcong <wenghengcong@gmail.com>
2016-03-21 09:54:38 +0800

    Added files via upload

---
+++ LifeCycle/LifeCycle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LifeCycle
+//
+//  Created by WengHengcong on 3/3/16.
+//  Copyright © 2016 WengHengcong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
