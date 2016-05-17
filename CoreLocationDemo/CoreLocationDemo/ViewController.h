ee1d6f5868d3d288769ba487cdabe3aa2d9f69e1
wenghengcong <wenghengcong@gmail.com>
2016-05-17 22:30:24 +0800

    corelocation demo

---
+++ CoreLocationDemo/CoreLocationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreLocationDemo
+//
+//  Created by WengHengcong on 5/8/16.
+//  Copyright © 2016 WengHengcong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
