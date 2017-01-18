a7dac99df2f24f035a28262d3173cc65e91a0d65
wenghengcong <wenghengcong@gamil.com>
2017-01-18 20:12:59 +0800

    完善demo

--- KVOLearnDemo/KVODemo/Model/Book.m
+++ KVOLearnDemo/KVODemo/Model/Book.m
@@ -10,4 +10,28 @@
 
 @implementation Book
 
+- (void)setPrice:(CGFloat)price
+{
+    [self willChangeValueForKey:@"price"];
+    _price = price;
+    [self didChangeValueForKey:@"price"];
+}
+
++ (BOOL)automaticallyNotifiesObserversForKey:(NSString *)key
+{
+    if ([key isEqualToString:@"bookName"]) {
+        return NO;
+    }
+    
+    return [super automaticallyNotifiesObserversForKey:key];
+}
+
+//设置属性依赖
+//如果bookName改变，那么观察者也会收到price改变的通知
++ (NSSet *)keyPathsForValuesAffectingBookName
+{
+    NSSet *set = [NSSet setWithObjects:@"price", nil];
+    return set;
+}
+
 @end
--- KVOLearnDemo/KVODemo/ViewController/ViewController.m
+++ KVOLearnDemo/KVODemo/ViewController/ViewController.m
@@ -10,6 +10,7 @@
 #import "Person.h"
 #import "Book.h"
 #import "SetterValueObject.h"
+#import <objc/runtime.h>
 
 @interface ViewController ()
 
@@ -51,9 +52,32 @@
     //键值监听,我很关心价格，价格一变，立即提醒
     [self.book  addObserver:self forKeyPath:@"price" options:NSKeyValueObservingOptionNew |NSKeyValueObservingOptionOld context:nil];
     
+    [self testDemos];
+    
 }
 
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context{
+- (void)testDemos
+{
+    [self textField];
+    [self arrayKVC];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object change:(NSDictionary *)change
+                       context:(void *)context{
+    
+    /**
+     *  object  监听的对象，即self.book（Book对象）
+     *  keyPath  监听的键：此处为price
+     *  content:自定义内容
+     *change:
+      {
+        kind = 1;          //类型
+        new = 2;           //新值
+        old = 10000;       //旧值
+      }
+     */
+    
     //触发监听方法
     UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"价格变了" message:@"" delegate:nil cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
     [alert show];
@@ -75,9 +99,38 @@
     /**
      *  移除监听
      */
+    
     [self.book removeObserver:self forKeyPath:@"price"];
     
 }
 
+- (void)textField
+{
+    UITextField *textField = [[UITextField alloc] init];
+    textField.frame = CGRectMake(0, 0, 300, 200);
+    textField.placeholder = @"我是小学生";
+    [textField setValue:[UIFont systemFontOfSize:25.0] forKeyPath:@"_placeholderLabel.font"];
+//    [self.view addSubview:textField];
+    
+    unsigned int count = 0;
+    objc_property_t *properties = class_copyPropertyList([UITextField class], &count);
+    for (int i = 0; i < count; i++) {
+        objc_property_t property = properties[i];
+        const char *name = property_getName(property);
+        NSLog(@"name:%s",name);
+    }
+}
+
+- (void)arrayKVC
+{
+    NSArray *arr = @[@"ali",@"bob",@"cydia"];
+    NSArray *arrCap = [arr valueForKey:@"capitalizedString"];
+    for (NSString *str  in arrCap) {
+        NSLog(@"%@",str);        //Ali\Bob\Cydia
+    }
+    
+    NSMutableDictionary *dic = @{};
+    NSString *d = [dic objectForKey:@""];
+}
 
 @end
