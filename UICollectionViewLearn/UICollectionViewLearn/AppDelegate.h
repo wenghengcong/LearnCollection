e3a197ad118e4def8d4f1c780900de1253004c2d
wenghengcong <wenghengcong@gamil.com>
2017-03-06 09:46:36 +0800

    UICollectionView

---
+++ UICollectionViewLearn/UICollectionViewLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionViewLearn
+//
+//  Created by WengHengcong on 2016/11/8.
+//  Copyright © 2016年 JungleSong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICollectionViewLearn/UICollectionViewLearn/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  UICollectionViewLearn
+//
+//  Created by WengHengcong on 2016/11/8.
+//  Copyright © 2016年 JungleSong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
