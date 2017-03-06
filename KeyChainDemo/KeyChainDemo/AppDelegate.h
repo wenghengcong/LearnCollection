d905d40aa6fd03750ae001188d26a29a3b60450f
wenghengcong <wenghengcong@gamil.com>
2017-03-06 10:19:09 +0800

    keychain demo

---
+++ KeyChainDemo/KeyChainDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyChainDemo
+//
+//  Created by WengHengcong on 16/5/26.
+//  Copyright © 2016年 WengHengcong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
