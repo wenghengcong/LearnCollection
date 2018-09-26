552ed8f9ffe6fd6bb29ba906dbfd101b4eb9e158
wenghengcong <wenghengcong@gamil.com>
2018-09-26 12:01:30 +0800

    算法书

---
+++ BOOK-Algorithhms 4th/myself/chap1_1_3.java
@@ -0,0 +1,16 @@
+public class chap1_1_3 {
+	public static void main(String[] args) {
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+		if (a == b && b == c) {
+			System.out.printf("equal");
+		} else {
+			System.out.printf("not equal");
+		}
+		if a > 0 
+		 c = 0;
+		else 
+		 c = 1;
+	}
+}
