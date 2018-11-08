05668e9cb0676516851b64b80336ed461fdd6715
wenghengcong <wenghengcong@gamil.com>
2018-11-08 19:09:19 +0800

    no message

---
+++ CLearn/CLearn/DataType.h
@@ -0,0 +1,16 @@
+//
+//  DataType.h
+//  CLearn
+//
+//  Created by 翁恒丛 on 2018/11/8.
+//  Copyright © 2018年 翁恒丛. All rights reserved.
+//
+
+#ifndef DataType_h
+#define DataType_h
+
+#include <stdio.h>
+
+void logDataTypeMemorySize();
+
+#endif /* DataType_h */
---
+++ CLearn/CLearn/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  CLearn
+//
+//  Created by 翁恒丛 on 2018/11/8.
+//  Copyright © 2018年 翁恒丛. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DataType.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        logDataTypeMemorySize();
+    }
+    return 0;
+}
+
+
