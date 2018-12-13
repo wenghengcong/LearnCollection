358013032c20b112ca6a5b4743e1bbde785ca407
wenghengcong <wenghengcong@gamil.com>
2018-12-13 22:55:19 +0800

    no message

---
+++ Runtime/方法缓存探索/方法缓存探索/BFBoy.h
@@ -0,0 +1,18 @@
+//
+//  BFBoy.h
+//  方法缓存
+//
+//  Created by WengHengcong on 2018/12/13.
+//  Copyright © 2018 WengHengcong. All rights reserved.
+//
+
+#import "BFPerson.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BFBoy : BFPerson
+
+- (void)boyTest;
+@end
+
+NS_ASSUME_NONNULL_END
