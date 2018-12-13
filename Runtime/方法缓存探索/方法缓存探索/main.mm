a0ea850d9e5f594e3a5e9547199d956ba1c34da0
wenghengcong <wenghengcong@gamil.com>
2018-12-13 23:14:22 +0800

    no message

--- Runtime/方法缓存探索/方法缓存探索/main.mm
+++ Runtime/方法缓存探索/方法缓存探索/main.mm
@@ -16,56 +16,52 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         //1. 观察方法缓存相关结构体的底层结构
-//        BFPerson *person = [[BFPerson alloc] init];
-//        struct bf_objc_class *personClass = (__bridge struct bf_objc_class *)[BFPerson class];
-//        [person personTest];
-//        [person eat];
-//        [person run];
-//
-//        cache_t personCache = personClass->cache;
-//        bucket_t *personBuckets = personCache._buckets;
-//
-//        [person personTest];
-//        [person eat];
-//        [person run];
-//
-//        bucket_t bucket = personBuckets[(long long)@selector(personTest) & personCache._mask];
-//        for (int i = 0; i <= personCache._mask; i++) {
-//            bucket_t bucket = personBuckets[i];
-//            NSLog(@"%s --- %p", bucket._key, bucket._imp);
-//        }
+        BFPerson *person = [[BFPerson alloc] init];
+        struct bf_objc_class *personClass = (__bridge struct bf_objc_class *)[BFPerson class];
+        [person personTest];
+        [person eat];
+        [person run];
         
-        //2. hash
-        BFTallBoy *tallBoy = [[BFTallBoy alloc] init];
-        struct bf_objc_class *tallBoyClass = (__bridge struct bf_objc_class *)[BFTallBoy class];
-        
-        [tallBoy tallBoyTest];
-        [tallBoy boyTest];
-        [tallBoy personTest];
-        
-        [tallBoy tallBoyTest];
-        [tallBoy boyTest];
-        [tallBoy personTest];
-        
+        cache_t personCache = personClass->cache;
+        bucket_t *personBuckets = personCache._buckets;
+        bucket_t bucket = personBuckets[(long long)@selector(personTest) & personCache._mask];
+        for (int i = 0; i <= personCache._mask; i++) {
+            bucket_t bucket = personBuckets[i];
+            NSLog(@"index:%d %s --- %p",i, bucket._key, bucket._imp);
+        }
         NSLog(@"=======================");
-        
-        cache_t tallBoyCache = tallBoyClass->cache;
-        bucket_t *tallBoyBuckets = tallBoyCache._buckets;
-        
-        //输出该类所有的缓存方法
-        for (int i = 0; i <= tallBoyCache._mask; i++) {
-            bucket_t bucket = tallBoyBuckets[i];
-            NSLog(@"index: %d:  %s - %p", i, bucket._key, bucket._imp);
-        }
-    
-        NSLog(@"%s %p", @selector(tallBoyTest), tallBoyCache.imp(@selector(tallBoyTest)));
-        NSLog(@"%s %p", @selector(boyTest), tallBoyCache.imp(@selector(boyTest)));
-        NSLog(@"%s %p", @selector(personTest), tallBoyCache.imp(@selector(personTest)));
-        
-        for (int i = 0; i <= tallBoyCache._mask; i++) {
-            bucket_t bucket = tallBoyBuckets[i];
-            NSLog(@"index: %d:  %s - %p", i, bucket._key, bucket._imp);
-        }
+
+        //2. hash
+//        BFTallBoy *tallBoy = [[BFTallBoy alloc] init];
+//        struct bf_objc_class *tallBoyClass = (__bridge struct bf_objc_class *)[BFTallBoy class];
+//
+//        [tallBoy tallBoyTest];
+//        [tallBoy boyTest];
+//        [tallBoy personTest];
+//
+//        [tallBoy tallBoyTest];
+//        [tallBoy boyTest];
+//        [tallBoy personTest];
+//
+//        NSLog(@"=======================");
+//
+//        cache_t tallBoyCache = tallBoyClass->cache;
+//        bucket_t *tallBoyBuckets = tallBoyCache._buckets;
+//
+//        //输出该类所有的缓存方法
+//        for (int i = 0; i <= tallBoyCache._mask; i++) {
+//            bucket_t bucket = tallBoyBuckets[i];
+//            NSLog(@"index: %d:  %s - %p", i, bucket._key, bucket._imp);
+//        }
+//
+//        NSLog(@"%s %p", @selector(tallBoyTest), tallBoyCache.imp(@selector(tallBoyTest)));
+//        NSLog(@"%s %p", @selector(boyTest), tallBoyCache.imp(@selector(boyTest)));
+//        NSLog(@"%s %p", @selector(personTest), tallBoyCache.imp(@selector(personTest)));
+//
+//        for (int i = 0; i <= tallBoyCache._mask; i++) {
+//            bucket_t bucket = tallBoyBuckets[i];
+//            NSLog(@"index: %d:  %s - %p", i, bucket._key, bucket._imp);
+//        }
         NSLog(@"=======================");
     }
     return 0;
