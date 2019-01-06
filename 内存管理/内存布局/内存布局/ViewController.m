23425f1b8a357c305b12d8b1e78f6d58d1c70a43
wenghengcong <wenghengcong@gamil.com>
2019-01-07 02:33:04 +0800

    引用计数部分完成

--- 内存管理/内存布局/内存布局/ViewController.m
+++ 内存管理/内存布局/内存布局/ViewController.m
@@ -14,19 +14,19 @@
 
 @implementation ViewController
 
-int a = 10;
-int b;
+int a = 10; // 全局变量：已初始化
+int b;      // 全局变量：未初始化
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    static int c = 20;
-    static int d;
-    int e;
-    int f = 20;
+    static int c = 20;  // 静态变量：已初始化
+    static int d;       // 静态变量：未初始化
+    int e;              // 自动变量：未初始化
+    int f = 20;         // 自动变量：已初始化
     
-    NSString *str = @"123";
-    NSObject *obj1 = [[NSObject alloc] init];
+    NSString *str = @"123";     // 字符串常量
+    NSObject *obj1 = [[NSObject alloc] init];       // 自动变量：对象变量
     NSObject *obj2 = [[NSObject alloc] init];
     NSLog(@"\n&a=%p\n&b=%p\n&c=%p\n&d=%p\n&e=%p\n&f=%p\nstr=%p\nobj1=%p\nobj2=%p\n",
           &a, &b, &c, &d, &e, &f, str, obj1, obj2);
@@ -34,22 +34,22 @@ int b;
 /*
  iOS 真机下地址
  字符串常量
- str = 0x10062c068
+ str = 0x102418068
  
  已初始化的全局变量、静态变量
- &a = 0x10062cd98
- &c = 0x10062cd9c
+ &a = 0x102418d98
+ &c = 0x102418d9c
  
  未初始化的全局变量、静态变量
- &d = 0x10062ce60
- &b = 0x10062ce64
+ &d = 0x102418e60
+ &b = 0x102418e64
  
  堆
  obj1 = 0x1c0002240
  obj2 = 0x1c0002200
  
  栈
- &f = 0x16f7d9358
- &e = 0x16f7d935c
+ &f = 0x16d9ed358
+ &e = 0x16d9ed35c
  */
 @end
